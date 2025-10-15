4f35afad00c3b174049755f50f40f3d6e7669a9c
RaphaelAnjos27 <raphael.anjos@outlook.com.br>
2025-10-15 12:11:01 -0300

    Primeira Versão completa do site

--- src/components/head.js
+++ src/components/head.js
@@ -10,14 +10,14 @@ export class EteccHead extends HTMLElement{
 		this.shadowRoot.innerHTML=
         `
 
-        <link href="/src/css/bootstrap.min.css" rel="stylesheet">
-        <script src="/src/js/bootstrap.bundle.min.js"></script>
+        <link href="src/css/bootstrap.min.css" rel="stylesheet">
+        <script src="src/js/bootstrap.bundle.min.js"></script>
 
         <div 
             class="p-5 text-white text-center"
-            style="background-color: #ec1b39; background-image: url('/static/pattern2.webp'); background-repeat: repeat; background-position: center; background-size: 100px;"
+            style="background-color: #ec1b39; background-image: url('static/pattern2.webp'); background-repeat: repeat; background-position: center; background-size: 100px;"
             >
-            <img src="/static/EteccLogo.webp" class="mx-auto d-block img-fluid w-50"
+            <img src="static/EteccLogo.webp" class="mx-auto d-block img-fluid w-50"
                 alt="ETECC logo in bold stylized letters with the word TELECOM below, set against a light background. The design conveys a modern and professional tone.">
             <br>
             <h2>ETECC Guia Telecom</h2>
